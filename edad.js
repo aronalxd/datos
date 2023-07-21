dbd1a85d83a6f552d4782de3333778f5fa744e24
aronalxd <55291528+aronalxd@users.noreply.github.com>
2023-07-21 10:49:35 -0500

    Create edad.js

---
+++ edad.js
@@ -0,0 +1,7 @@
+ <script>
+        function obtenerEdad() {
+            const edad = document.getElementById('edadInput').value;
+            console.log('Edad ingresada:', edad);
+            // Aquí puedes hacer lo que quieras con la variable 'edad'
+        }
+    </script>
